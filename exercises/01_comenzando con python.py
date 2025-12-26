8d83810c522a86881d5a87e61323884b2d3be69a
Daniel Macias <jmacias@comunidad.unam.mx>
2025-12-26 09:25:49 -0600

    Ejercicios print

---
+++ exercises/01_comenzando con python.py
@@ -0,0 +1,20 @@
+print ("Escuela de datos de Alura")
+
+#Imprimir nombre y apellido
+print ("Nombre: Daniel")
+print ("Apellido: Macias")
+
+#Imprimir nombre letra por letra
+print ("D")
+print ("A")
+print ("N")
+print ("I")
+print ("E")
+print ("L")
+
+#Imprimir fecha nacimiento con formato dia mes año
+
+print ("07 agosto 1992")
+
+#5 - Imprime, en una sola instrucción print, el año actual en el que estás realizando este curso. El valor del año debe ser un dato numérico, y la salida de print debe ser la siguiente:
+print ('Año actual:', 2025)
