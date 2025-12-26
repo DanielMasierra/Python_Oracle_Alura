4e89bbb49d19bf2b013bee46549c5b88fb8096bf
Daniel Macias <jmacias@comunidad.unam.mx>
2025-12-26 10:04:34 -0600

    Add variable types example

---
+++ core/control_flow/variables_and_types.py
@@ -0,0 +1,25 @@
+"""
+Tema: Tipos de variables en Python
+Contexto: Registro de una estudiante en una institución educativa
+
+Tipos cubiertos:
+- int
+- float
+- str
+- bool
+"""
+
+# Datos del estudiante
+nombre_estudiante = "Penelope Camacho"     # str
+edad_estudiante = 11                       # int
+promedio_estudiante = 9.75                 # float
+aprobada = True                            # bool
+
+# Mostrar valores
+print(nombre_estudiante, edad_estudiante, promedio_estudiante, aprobada)
+
+# Verificar tipos
+print(type(nombre_estudiante))
+print(type(edad_estudiante))
+print(type(promedio_estudiante))
+print(type(aprobada))
